c4f3f9e951608b21abe193f36109fe66ad609efa
ykpark <read0more@gmail.com>
2021-02-12 18:21:02 +0900

    Update: sqlite에서 postgresql로 변경

--- prisma/migrations/20210212090136_/migration.sql
+++ prisma/migrations/20210212090136_/migration.sql
@@ -1,36 +1,41 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "githubId" TEXT NOT NULL,
     "username" TEXT NOT NULL,
-    "profileImage" TEXT
+    "profileImage" TEXT,
+
+    PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "UserRepository" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "userId" INTEGER NOT NULL,
     "repositoryId" INTEGER NOT NULL,
     "repositoryUrl" TEXT NOT NULL,
-    FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY ("repositoryId") REFERENCES "Repository" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Repository" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "updatedAt" DATETIME NOT NULL
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "RepositoryVersion" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "repositoryId" INTEGER NOT NULL,
     "prerelease" BOOLEAN NOT NULL,
     "url" TEXT NOT NULL,
-    "publishedAt" DATETIME NOT NULL,
-    FOREIGN KEY ("repositoryId") REFERENCES "Repository" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "publishedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -41,3 +46,12 @@ CREATE UNIQUE INDEX "UserRepository.userId_repositoryId_unique" ON "UserReposito
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Repository.name_unique" ON "Repository"("name");
+
+-- AddForeignKey
+ALTER TABLE "UserRepository" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRepository" ADD FOREIGN KEY ("repositoryId") REFERENCES "Repository"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RepositoryVersion" ADD FOREIGN KEY ("repositoryId") REFERENCES "Repository"("id") ON DELETE CASCADE ON UPDATE CASCADE;
